ca0d37ebeef890596d1cb5c903d917e8b1d47f01
JulianRenaud <midway6220@gmail.com>
2022-09-22 11:23:22 -0500

    First Commit

---
+++ ClassesExercise/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesExercise
+{
+    internal class Car
+    {
+        
+
+
+
+        public string Brand { get; set; }
+        public string Price { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public int Horsepower { get; set; }
+
+       
+
+
+    }
+}
---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using ClassesExercise;
+using System.Diagnostics;
+using System.Reflection;
+
+var Car_1 = new Car();
+
+Car_1.Price = "$123,000";
+Car_1.Brand = "Corvette";
+Car_1.Model = "ZR1";
+Car_1.Year = 2022;
+Car_1.Horsepower = 755;
+
+
+
+Console.WriteLine($"Starting Price: {Car_1.Price}");
+Console.WriteLine($"Year: {Car_1.Year}");
+Console.WriteLine($"Brand: {Car_1.Brand}");
+Console.WriteLine($"Model: {Car_1.Model}");
+Console.WriteLine($"Horsepower: {Car_1.Horsepower}");
